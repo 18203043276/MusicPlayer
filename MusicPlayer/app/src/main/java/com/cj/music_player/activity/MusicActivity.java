30f72d9fe93d7333e604349c0b138d1ee9f2c48d
18203043276 <1607686294@qq.com>
2016-10-16 11:12:06 +0800

    音乐

--- MusicPlayer/app/src/main/java/com/cj/music_player/activity/MusicActivity.java
+++ MusicPlayer/app/src/main/java/com/cj/music_player/activity/MusicActivity.java
@@ -137,6 +137,7 @@ public class MusicActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(R.drawable.actionbar_home);
 
+        //侧滑
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
